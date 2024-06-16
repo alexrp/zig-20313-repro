3ea3b7f6a94b9154cb9ca2817dcc9b6dced00a14
Alex Rønne Petersen <alex@alexrp.com>
2024-06-16 06:19:21 +0200

    Revert "reduce"
    This reverts commit 25a42dd9ef7425d285a8a249db6fd1c6fbb1851b.

--- build.zig
+++ build.zig
@@ -4,6 +4,16 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const clap_mod = b.dependency("clap", .{
+        .target = target,
+        .optimize = optimize,
+    }).module("clap");
+
+    const mecha_mod = b.dependency("mecha", .{
+        .target = target,
+        .optimize = optimize,
+    }).module("mecha");
+
     const lib = b.addStaticLibrary(.{
         .name = "zig-20313-repro",
         .root_source_file = b.path("src/root.zig"),
@@ -17,6 +27,9 @@ pub fn build(b: *std.Build) void {
     })) |dep| {
         lib.root_module.addImport("aro", dep.module("aro"));
     }
+
+    lib.root_module.addImport("clap", clap_mod);
+    lib.root_module.addImport("mecha", mecha_mod);
 
     b.installArtifact(lib);
 }
