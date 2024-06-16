77d9783048bd228be72ce0fef28c44bed7c7e3fa
Alex Rønne Petersen <alex@alexrp.com>
2024-06-16 06:19:38 +0200

    reduce

--- build.zig
+++ build.zig
@@ -9,11 +9,6 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     }).module("clap");
 
-    const mecha_mod = b.dependency("mecha", .{
-        .target = target,
-        .optimize = optimize,
-    }).module("mecha");
-
     const lib = b.addStaticLibrary(.{
         .name = "zig-20313-repro",
         .root_source_file = b.path("src/root.zig"),
@@ -29,7 +24,6 @@ pub fn build(b: *std.Build) void {
     }
 
     lib.root_module.addImport("clap", clap_mod);
-    lib.root_module.addImport("mecha", mecha_mod);
 
     b.installArtifact(lib);
 }
